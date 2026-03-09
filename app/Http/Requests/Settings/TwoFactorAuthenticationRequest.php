7625e18e0ac87e72e3a863d6fc5937635fa5370f
Vinícius Boschetti <chagas.vb@gmail.com>
2026-03-09 07:19:58 -0300

    fix(HEY-2): format code with laravel pint

--- app/Http/Requests/Settings/TwoFactorAuthenticationRequest.php
+++ app/Http/Requests/Settings/TwoFactorAuthenticationRequest.php
@@ -4,8 +4,7 @@ namespace App\Http\Requests\Settings;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
-use Laravel\Fortify\Features;
-use Laravel\Fortify\InteractsWithTwoFactorState;
+use Laravel\Fortify\{Features, InteractsWithTwoFactorState};
 
 class TwoFactorAuthenticationRequest extends FormRequest
 {
