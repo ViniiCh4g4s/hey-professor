f2743f2c9174de03e052b5fcd43f130848ee0fb2
Vinícius Boschetti <chagas.vb@gmail.com>
2026-03-11 16:18:08 -0300

    feat(HEY-9): add conditional route for local development

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,17 @@ use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+if (app()->isLocal()) {
+    Route::get('/', function () {
+        auth()->loginUsingId(1);
+
+        return redirect()->route('dashboard');
+    })->name('home');
+} else {
+    Route::inertia('/', 'welcome', [
+        'canRegister' => Features::enabled(Features::registration()),
+    ])->name('home');
+}
 
 Route::post('question/store', [QuestionController::class, 'store'])->name('questions.store');
 
